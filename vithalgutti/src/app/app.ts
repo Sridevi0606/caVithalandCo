3dc09839cf5cd1ff828589c4a619a3dbe88033a3
Sridevi0606 <dixitsridevi@gmail.com>
2026-06-12 18:38:04 +0530

    third commit

--- vithalgutti/src/app/app.ts
+++ vithalgutti/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { Contact } from "./pages/contact/contact";
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterOutlet, RouterModule, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
--- vithalgutti/src/app/navbar/navbar.ts
+++ vithalgutti/src/app/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './navbar.html'
 })
 export class Navbar {}
